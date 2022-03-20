08331101e2da9989ffc8bff4d68a1b943336a458
mkmk0723 <mkmk0723@gmail.com>
2022-03-21 06:43:12 +0900

    slick追加

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ $(".menu a").click(function() {
 
 });
 
-$(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });
+$(window).scroll(function () {
+	fadeAnime();/* 関数を呼ぶ*/
+});
 function fadeAnime(){
 // fadeUp
 $('.fade-in-up').each(function(){ 
@@ -33,42 +33,69 @@ $('.fade-in-up').each(function(){
 	var scroll = $(window).scrollTop();
 	var windowHeight = $(window).height();
 	if (scroll >= elemPos - windowHeight){
-	$(this).addClass('fade-in');
-	$(this).removeClass('fade-in');
+	$(this).addClass('fadeUp');
+	}else{
+	$(this).removeClass('fadeUp');
 	}
-}); 
+});  
 // fadeDown
 $('.fade-in-down').each(function(){ 
 	var elemPos = $(this).offset().top-50;
 	var scroll = $(window).scrollTop();
 	var windowHeight = $(window).height();
 	if (scroll >= elemPos - windowHeight){
-	$(this).addClass('fade-in');
+	$(this).addClass('fadeDown');
 	}else{
-	$(this).removeClass('fade-in');
+	$(this).removeClass('fadeDown');
 	}
 }); 
 
 // fadeRight
-  $('.fade-in-right').each(function(){
-    var elemPos = $(this).offset().top-50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fade-in');
-    }else{
-    $(this).removeClass('fade-in');
-    }
-});
+$('.fade-in-left').each(function(){ 
+	var elemPos = $(this).offset().top-50;
+	var scroll = $(window).scrollTop();
+	var windowHeight = $(window).height();
+	if (scroll >= elemPos - windowHeight){
+	$(this).addClass('fadeLeft');
+	}else{
+	$(this).removeClass('fadeLeft');
+	}
+}); 
 // fadeLeft
-$('.fade-in-left').each(function(){
+$('.fade-in-right').each(function(){
   var elemPos = $(this).offset().top-50;//要素より、50px上の
   var scroll = $(window).scrollTop();
   var windowHeight = $(window).height();
   if (scroll >= elemPos - windowHeight){
-  $(this).addClass('fade-in');
+  $(this).addClass('fadeRight');
   }else{
-  $(this).removeClass('fade-in');
+  $(this).removeClass('fadeRight');
   }
 });
 }
+// 
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  variableWidth:true,
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});

389612ad071abfc4af9c0a0ef1726a977f0a15c6
mkmk0723 <mkmk0723@gmail.com>
2022-03-20 16:03:58 +0900

    jQuery

--- js/script.js
+++ js/script.js
@@ -42,19 +42,6 @@ $('.fade').each(function(){ //fadeUpTriggerというクラス名が
   });
 }
 
-function fadeLeftAnime(){
-// fadeLeft
-$('.fade').each(function(){ //fadeLeftTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeLeft');// 画面内に入ったらfadeLeftというクラス名を追記
-    }else{
-    $(this).removeClass('fadeLeft');// 画面外に出たらfadeLeftというクラス名を外す
-    }
-});
-};
 
 $(window).scroll(function (){
     fadeRightAnime();/* アニメーション用の関数を呼ぶ*/
